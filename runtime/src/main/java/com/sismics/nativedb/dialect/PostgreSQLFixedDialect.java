c3f326ec08084150d1a77ddb936612d0d0fee291
Jean-Marc Tremeaux <jm.tremeaux@sismics.com>
2020-03-29 15:29:39 +0200

    Use latest PostgreSQL dialect

--- runtime/src/main/java/com/sismics/nativedb/dialect/PostgreSQLFixedDialect.java
+++ runtime/src/main/java/com/sismics/nativedb/dialect/PostgreSQLFixedDialect.java
@@ -3,7 +3,7 @@ package com.sismics.nativedb.dialect;
 import org.hibernate.boot.model.TypeContributions;
 import org.hibernate.service.ServiceRegistry;
 
-public class PostgreSQLFixedDialect extends org.hibernate.dialect.PostgreSQL82Dialect {
+public class PostgreSQLFixedDialect extends org.hibernate.dialect.PostgreSQL95Dialect {
     @Override
     public void contributeTypes(TypeContributions typeContributions, ServiceRegistry serviceRegistry) {
         super.contributeTypes(typeContributions, serviceRegistry);
